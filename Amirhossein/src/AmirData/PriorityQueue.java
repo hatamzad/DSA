1f19b6c0abf515b8847cc321046ea951038b37e3
Amirhossein Hatamzad <44765937+hatamzad@users.noreply.github.com>
2020-05-05 15:48:37 -0700

    Add files via upload

---
+++ Amirhossein/src/AmirData/PriorityQueue.java
@@ -0,0 +1,38 @@
+package AmirData;
+
+import java.util.Arrays;
+
+public class PriorityQueue {
+   private int[] items = new int[5];
+   private int count;
+
+   public void add(int item) {
+       if (count == items.length)
+           throw new IllegalStateException();
+       //shifting
+       int i;
+       for (i = count - 1; i >= 0; i--) {
+           if (items[i] > item)
+               items[i + 1] = items[i];
+           else
+               break;
+       }
+       items[i + 1] = item;
+       count++;
+   }
+   public int remove() {
+       if (isEmpty())
+           throw new IllegalStateException();
+
+       return items[--count];
+   }
+
+   public boolean isEmpty() {
+       return count == 0;
+   }
+
+   @Override
+   public String toString() {
+       return Arrays.toString(items);
+   }
+}
